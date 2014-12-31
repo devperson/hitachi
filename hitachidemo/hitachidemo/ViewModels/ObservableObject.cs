614c8ae2a53849945902f5124e78470b3bc42faf
Xusan <xusan@gmail.fd>
2014-12-31 18:58:27 +0500

    fgf

---
+++ hitachidemo/hitachidemo/ViewModels/ObservableObject.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HitachiDemo.ViewModels
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }       
+    }
+
+    public static class ObservableBaseEx
+    {
+        //override 1
+        public static void RaisePropertyChanged<T, TProperty>(this T observableBase, Expression<Func<T, TProperty>> expression) where T : ObservableObject
+        {
+            observableBase.RaisePropertyChanged(observableBase.GetPropertyName(expression));
+        }
+
+        public static string GetPropertyName<T, TProperty>(this T owner, Expression<Func<T, TProperty>> expression)
+        {
+            var memberExpression = expression.Body as MemberExpression;
+            if (memberExpression == null)
+            {
+                var unaryExpression = expression.Body as UnaryExpression;
+                if (unaryExpression != null)
+                {
+                    memberExpression = unaryExpression.Operand as MemberExpression;
+                    if (memberExpression == null)
+                        throw new NotImplementedException();
+                }
+                else
+                    throw new NotImplementedException();
+            }
+
+            var propertyName = memberExpression.Member.Name;
+            return propertyName;
+        }
+    }
+}
---
+++ hitachidemo/hitachidemo/ViewModels/ScreensViewModel.cs
@@ -0,0 +1,48 @@
+﻿using HitachiDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HitachiDemo.ViewModels
+{
+    public class ScreensViewModel : ObservableObject
+    {
+
+        ObservableCollection<ScreenItem> _screens;
+        public ObservableCollection<ScreenItem> Screens
+        {
+            get
+            {
+                return _screens;
+            }
+            set
+            {
+                _screens = value;
+                this.RaisePropertyChanged(p => p.Screens);
+            }
+        }
+
+        
+
+        public ScreensViewModel()
+        {
+            this.GenereateScreens();
+        }
+
+        private void GenereateScreens()
+        {
+            this.Screens = new ObservableCollection<ScreenItem>();
+
+            this.Screens.Add(new ScreenItem { Text = "Your account page", ImageName = "sample.jpg" });
+            this.Screens.Add(new ScreenItem { Text = "VIP Members Club", ImageName = "sample.jpg" });
+            this.Screens.Add(new ScreenItem { Text = "Make reservations", ImageName = "sample.jpg" });
+            this.Screens.Add(new ScreenItem { Text = "Your Favorites", ImageName = "sample.jpg" });
+            this.Screens.Add(new ScreenItem { Text = "Messages", ImageName = "sample.jpg" });
+            this.Screens.Add(new ScreenItem { Text = "Get a gift card", ImageName = "sample.jpg" });
+
+        }
+    }
+}
